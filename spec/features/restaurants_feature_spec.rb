ad4c62db3a9ddbc7f051952854cc32fa9d245eb5
“DovileSand” <dovile.sand@protonmail.ch>
2015-11-23 16:19:39 +0000

    Created second feature test and Restaurant class

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'restaurants' do
+feature 'restaurants:' do
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
@@ -8,4 +8,17 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'Nandos')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('Nandos')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+
 end
